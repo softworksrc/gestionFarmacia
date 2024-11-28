474909fa4b183c0c0310ed77b9001c032060b2a9
softworksrc <softworksrc@gmail.com>
2024-11-27 18:36:25 -0600

    m

--- src/app/views/gestionFarmacia/admin/productoMalEstado/inventario-producto-mal-estado/inventario-producto-mal-estado.component.ts
+++ src/app/views/gestionFarmacia/admin/productoMalEstado/inventario-producto-mal-estado/inventario-producto-mal-estado.component.ts
@@ -41,16 +41,11 @@ export class InventarioProductoMalEstadoComponent implements OnInit {
       console.error('Error al actualizar el estado de existencia', error);
     });
   }
-
-  // Marcar el producto como "vendido"
   marcarVendido(producto: any): void {
-    // No se elimina de inmediato, solo se marca como vendido
     if (producto.vendido) {
       console.log(`Producto ${producto.codigo} marcado como vendido.`);
     }
   }
-
-  // Filtrar productos por código o nombre
   filtrarProductos(): void {
     const searchTermLower = this.searchTerm.toLowerCase();
     this.productosFiltrados = this.productos.filter(producto =>
@@ -58,43 +53,29 @@ export class InventarioProductoMalEstadoComponent implements OnInit {
       producto.producto.toLowerCase().includes(searchTermLower)
     );
   }
-
   guardarDatos(): void {
     const carpeta = 'productosMalEstado';
     const productosVendidos = this.productos.filter(producto => producto.vendido);
-
-    // Eliminar los productos vendidos
     const eliminarVendidos = productosVendidos.map(producto => {
       return this.firebaseService.eliminar(producto.id, carpeta);
     });
-
-    // Desmarcar los productos existentes
     const desmarcarExistencia = this.productos.filter(producto => producto.existencia).map(producto => {
       producto.existencia = false;
       return this.firebaseService.editar(carpeta, producto);
     });
-
-    // Unimos todas las promesas (eliminar + desmarcar)
     const todasLasOperaciones = [...eliminarVendidos, ...desmarcarExistencia];
-
-    // Ejecutamos todas las promesas
     Promise.all(todasLasOperaciones)
       .then(() => {
-        // Si todas las operaciones fueron exitosas, mostramos el mensaje de éxito
         alertExito('Operación exitosa', 'Los productos vendidos han sido eliminados y los existentes desmarcados correctamente.', this.router);
-        
-        // Actualizamos la lista visualmente sin recargar la página
-        this.productos = this.productos.filter(producto => !producto.vendido); // Eliminar los productos vendidos de la vista
-        this.filtrarProductos(); // Volver a filtrar la lista
+        this.productos = this.productos.filter(producto => !producto.vendido); 
+        this.filtrarProductos(); 
       })
       .catch(() => {
-        // Si alguna operación falla, mostramos el mensaje de error
         alertError('Hubo un error al guardar los datos. Por favor, intenta nuevamente.');
       });
   }
 
   regresar(): void {
-    // Redirigir a la página principal
     this.router.navigate(['/menu-administrativo']);
   }
 }
